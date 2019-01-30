d23920e9fe71108b8e5cbe044a4812b7a55cb972
Kevin Hu <hu.kevin.w@gmail.com>
2019-01-30 16:30:44 -0500

    Pushing to github

---
+++ popup.js
@@ -0,0 +1,15 @@
+// Future JavaScript will go here
+console.clear();
+console.log(localStorage);
+let data = {
+  daily: ['do1', 'do2']
+};
+
+
+
+setTimeout(function() {
+  document
+    .querySelector('input[type="checkbox"]')
+    .setAttribute('checked', true);
+}, 100);
+
