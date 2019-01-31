551b46a9c2e2adc1ff85312245d6eba02774d05c
Kevin Hu <hu.kevin.w@gmail.com>
2019-01-31 17:00:29 -0500

    Has persistance data

--- popup.js
+++ popup.js
@@ -1,14 +1,12 @@
-// Future JavaScript will go here
 console.clear();
-console.log(localStorage);
-
-
-
-
+const appName = 'daily-goal';
 class Task {
-  constructor(desc) {
-    this.desc = desc
-    this.fin = false
+  constructor(desc, fin = false) {
+    this.desc = desc;
+    this.fin = fin;
+    this.id = Math.random()
+      .toString(36)
+      .substr(2, 16);
   }
 
   toggleFin() {
@@ -16,15 +14,14 @@ class Task {
   }
 }
 
-
-
-
-const createTodoEle = (text) => {
-  const todo = document.createElement('div');
+const createTodoEle = obj => {
+  const todo = document.createElement('label');
+  const att = document.createAttribute('key');
+  att.value = obj.id;
+  todo.classList.add('todo');
+  todo.setAttributeNode(att);
   todo.innerHTML = `
-    <label class="todo">
-      <input class="todo__state" type="checkbox" />
-
+      <input class="todo__state" type="checkbox" ${obj.fin ? 'checked' : ''}/>
       <svg
         xmlns="http://www.w3.org/2000/svg"
         xmlns:xlink="http://www.w3.org/1999/xlink"
@@ -36,47 +33,81 @@ const createTodoEle = (text) => {
         <use xlink:href="#todo__check" class="todo__check"></use>
         <use xlink:href="#todo__circle" class="todo__circle"></use>
       </svg>
-
-      <div class="todo__text">${text}</div>
-      <span class="todo__close" action="delete-todo">❌</span>
-    </label>
+      <div class="todo__text">${obj.desc}</div>
+      <span class="todo__close" action="delete-todo">x</span>
   `;
-  return todo
-}
-const appendEleToTodoListEle = (domEle) => {
+  return todo;
+};
+const appendEleToTodoListEle = domEle => {
   let todoList = document.getElementById('todo-list');
-  todoList.appendChild( domEle );
-}
+  todoList.appendChild(domEle);
+};
+const dataIntoTodoList = tasks => {
+  tasks.forEach(task => {
+    appendEleToTodoListEle(createTodoEle(task));
+  });
+};
+const updateLocalStorage = obj => {
+  localStorage.setItem(appName, JSON.stringify(obj));
+};
+const onClickFunction = event => {
+  const { target } = event;
+  const btnAction = target.attributes.action
+    ? target.attributes.action.value
+    : null;
+  const todoId = target.parentElement.attributes.key.value;
+  // if (btnAction === 'add-to-do') {
+  //   addToDo();
+  // }
 
-function dataIntoTodoList(tasks) {
-  tasks.forEach( task => {
-    console.log( createTodoEle (task.desc) )
-    appendEleToTodoListEle( createTodoEle( task.desc ) )
-  })
-}
-// let data = {
-//   daily: [ 
-//     new Task('do stuff'),
-//     new Task('do stuff'),
-//     new Task('do stuff'),
-//   ]
-// };
+  if (btnAction === 'delete-todo') {
+    // add class for animation
+    // when animation finished, remove ele node
+    target.parentElement.classList.add('hide');
+    setTimeout(function() {
+      // edit data,
+      // update data to localstorage
+      // remove clicked todo ele
+      let storeCopy = { ...store };
+      storeCopy.daily = storeCopy.daily.filter(todo => todo.id != todoId);
+      updateLocalStorage(storeCopy);
+      target.parentElement.remove();
+    }, 500);
+  } else {
+    // clicked anywhere other than the delete btn
+    // update localstorage
+    let storeCopy = { ...store };
+    storeCopy.daily.forEach(todo => {
+      if (todo.id === todoId) {
+        todo.fin = !todo.fin;
+      }
+    });
+    updateLocalStorage(storeCopy);
+  }
+};
 
-// dataIntoTodoList(data.daily);
+// DOM elements
+const todoList = document.getElementById('todo-list');
+const todos = document.getElementsByClassName('todo');
+const store = localStorage.getItem(appName)
+  ? JSON.parse(localStorage.getItem(appName))
+  : null;
 
-if ( localStorage.getItem('dail-goal') ) {
-  // when there are stored data 
-  let data = JSON.parse( localStorage.getItem('dail-goal') );
-  dataIntoTodoList(data.daily);
+// data is stored in local storage
+if (store) {
+  // when there are stored data
+  dataIntoTodoList(store.daily);
 } else {
-  // no stored data 
+  // no stored data; test
   let data = {
-    daily: [ 
+    daily: [
+      new Task('Test task', true),
       new Task('do stuff'),
       new Task('do stuff'),
-      new Task('do stuff'),
-    ]
+    ],
   };
-  localStorage.setItem('dail-goal', JSON.stringify(data) )
+  localStorage.setItem(appName, JSON.stringify(data));
 }
 
+// event listeners
+todoList.addEventListener('click', onClickFunction);
